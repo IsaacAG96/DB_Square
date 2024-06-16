ec934f979736a0f84efb3edc951ad7adbebc694f
IsaacAG96 <isaacaberturasgalea@gmail.com>
2024-06-16 02:27:42 +0200

    boton volver corregido y redireccion a editar

--- nombre-del-proyecto/resources/views/table/view.blade.php
+++ nombre-del-proyecto/resources/views/table/view.blade.php
@@ -125,8 +125,11 @@
                 </a>
             </div>
             <div class="mt-4 text-right">
-                <a href="{{ url()->previous() }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                <a href="{{ url('/menu/gestionar') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
                     {{__('Back')}}
+                </a>
+                <a href="{{ route('table.edit', ['table' => $table]) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                    {{__('Edit Table')}}
                 </a>
             </div>
         </div>
